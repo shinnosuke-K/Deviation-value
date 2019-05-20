b105d8b52f949a5b9af01965cfd4153d14d4b35c
shinnosuke-K <shin.050195@gmail.com>
2019-05-20 13:53:16 +0900

    change: apply vue.js
    点数、偏差値、平均点の入力欄にVue.jsを適用してjsから値操作可能

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@ new Vue({
     el: '#app',
     data: {
         key: true,
+        testScore: 50,
+        deviationValue: 50.0,
+        averageScore: 50.0,
     },
     methods: {
         onChange(event) {
