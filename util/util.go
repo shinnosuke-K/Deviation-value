98412cdefe4200498db982d549f48a440bd6b823
shinnosuke-K <shin.050195@gmail.com>
2020-03-20 19:38:42 +0900

    create function
    ポート番号を取得する関数を作成

--- util/util.go
+++ util/util.go
@@ -8,3 +8,11 @@ func GetHost() string {
 	}
 	return "host=db user=prefectures password=root dbname=prefectures port=5432 sslmode=disable"
 }
+
+func GetPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "8080"
+	}
+	return port
+}
