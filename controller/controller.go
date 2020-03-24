863c92a9972c1f69b73006bf8e5eb6b26a824012
shinnosuke-K <shin.050195@gmail.com>
2020-03-24 14:55:05 +0900

    add process
    ページが表示されたときに参照される処理を追加

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -10,4 +12,16 @@ type Controller struct {
 
 func NewController(d *gorm.DB) *Controller {
 	return &Controller{db: d}
+}
+
+func (ctr *Controller) FileHandle() http.Handler {
+	return http.FileServer(http.Dir("templates"))
+}
+
+func (ctr *Controller) AssetsHandle() http.Handler {
+	return http.StripPrefix("/assets/", http.FileServer(http.Dir("assets")))
+}
+
+func (ctr *Controller) FaviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "assets/images/favicon.ico")
 }
